e230e4edd420d3b95fa39cbd0b6eb6a7701322c3
TOM-ATIV9\Tom <thomyerk@gmail.com>
2021-02-03 19:55:16 +0100

    pagination added

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -1,18 +1,35 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./style.css"
+import "./App.css"
 import { useCharacters, useLocations } from "./api/useData"
-import Logo from "./components/logo"
-import Buttons from "./components/buttons"
-import Infotext from "./components/description"
+import Logo from "./components/Logo"
+import Buttons from "./components/Buttons"
+import Infotext from "./components/Description"
+import ReactPaginate from "react-paginate"
 
 function App() {
-	const characters = useCharacters(1)
 	const locations = useLocations(1)
 
+	const [page, setPage] = useState(1)
+	const [offset, setOffset] = useState(1)
+	const [pageCount, setPageCount] = useState(34)
+
+	const characters = useCharacters(page)
+
+	const handlePageClick = (e) => {
+		setPage(e.selected)
+	}
+
+	// useEffect(() => {
+	// 	useCharacters(offset)
+	// }, [offset])
+
 	let charactersFetched = []
+	let pages = 0
 	if (characters != "Loading...") {
 		charactersFetched = characters.results
-		console.log(characters)
+		pages = characters.info.pages
+		console.log(pages)
 	}
 
 	return (
@@ -28,10 +45,22 @@ function App() {
 					<p>{character.species}</p>
 				</div>
 			))}
+			<ReactPaginate
+				previousLabel={"prev"}
+				nextLabel={"next"}
+				breakLabel={"..."}
+				breakClassName={"break-me"}
+				pageCount={pageCount}
+				marginPagesDisplayed={2}
+				pageRangeDisplayed={5}
+				onPageChange={handlePageClick}
+				containerClassName={"pagination"}
+				subContainerClassName={"pages pagination"}
+				activeClassName={"active"}
+				onClick={(e) => handlePageClick(e)}
+			/>
 		</div>
 	)
 }
-
-function CharacterList() {}
 
 export default App
--- react-and-morty/src/components/description.js
+++ react-and-morty/src/components/description.js
@@ -4,11 +4,11 @@ const Description = () => {
 		<div id="desc">
 			<p>Do you know Rick and Morty? If not, start watching it!</p>
 			<p>
-				Anyways, the Rick and Morty universe is kinda big with lot of
+				Anyways, the Rick and Morty universe is kinda big with a lot of
 				characters and locations. You with your friends decided that you
 				want to make it easier to look up information about each
-				characters and locations. You want to create a React app that
-				with the use of an open-source API, shows information about
+				character and location. You want to create a React app that with
+				the use of an open-source API, shows information about
 				everything you need. Your friend built the tools for the data
 				fetching, you just need to use the data and build the app.
 			</p>
