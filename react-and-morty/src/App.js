878475bc26aac2163e9bef0ea79b70bdccd547e4
evagurubi <evagurubi@gmail.com>
2021-02-04 18:46:06 +0100

    Styling nearly done.

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -1,73 +1,74 @@
-import React, { useState, useEffect } from "react"
-import "./style.css"
-import "./App.css"
-import { useCharacters, useLocations } from "./api/useData"
-import Logo from "./components/Logo"
-import Infotext from "./components/Description"
-import Characters from "./components/Characters"
-import Locations from "./components/Locations"
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import "./App.css";
+import { useCharacters, useLocations } from "./api/useData";
+import Logo from "./components/Logo";
+import Infotext from "./components/Description";
+import Characters from "./components/Characters";
+import Locations from "./components/Locations";
 
 function App() {
-	let charactersButton = process.env.PUBLIC_URL + "/img/characters.jpg"
-	let locationsButton = process.env.PUBLIC_URL + "/img/locations.jpg"
-	const [characterPages, setCharacterPages] = useState(1)
-	const [locationPages, setLocationPages] = useState(1)
-	//const [offset, setOffset] = useState(1)
-	const [content, setContent] = useState("description")
+  let charactersButton = process.env.PUBLIC_URL + "/img/characters.jpg";
+  let locationsButton = process.env.PUBLIC_URL + "/img/locations.jpg";
+  const [characterPages, setCharacterPages] = useState(1);
+  const [locationPages, setLocationPages] = useState(1);
+  //const [offset, setOffset] = useState(1)
+  const [content, setContent] = useState("description");
 
-	const locations = useLocations(locationPages)
-	const characters = useCharacters(characterPages)
+  const locations = useLocations(locationPages);
+  const characters = useCharacters(characterPages);
 
-	const handlePageClick = (e) => {
-		content === "characters"
-			? setCharacterPages(e.selected + 1)
-			: setLocationPages(e.selected + 1)
-	}
+  const handlePageClick = (e) => {
+    content === "characters"
+      ? setCharacterPages(e.selected + 1)
+      : setLocationPages(e.selected + 1);
+    window.scrollTo(0, 0);
+  };
 
-	// useEffect(() => {
-	// 	useCharacters(offset)
-	// }, [offset])
+  // useEffect(() => {
+  // 	useCharacters(offset)
+  // }, [offset])
 
-	let charactersFetched = []
-	let locationsFetched = []
+  let charactersFetched = [];
+  let locationsFetched = [];
 
-	if (characters !== "Loading...") {
-		charactersFetched = characters
-	}
-	if (locations !== "Loading...") {
-		locationsFetched = locations
-	}
-	return (
-		<div className="App">
-			<Logo onClick={() => setContent("description")} />
-			<div id="openbuttons">
-				<img
-					src={charactersButton}
-					alt={"charactersButton"}
-					onClick={() => setContent("characters")}
-				/>
-				<img
-					src={locationsButton}
-					alt={"locationsButton"}
-					onClick={() => setContent("locations")}
-				/>
-			</div>
+  if (characters !== "Loading...") {
+    charactersFetched = characters;
+  }
+  if (locations !== "Loading...") {
+    locationsFetched = locations;
+  }
+  return (
+    <div className="App">
+      <Logo onClick={() => setContent("description")} />
+      <div id="openbuttons">
+        <img
+          src={charactersButton}
+          alt={"charactersButton"}
+          onClick={() => setContent("characters")}
+        />
+        <img
+          src={locationsButton}
+          alt={"locationsButton"}
+          onClick={() => setContent("locations")}
+        />
+      </div>
 
-			{content === "characters" ? (
-				<Characters
-					characters={charactersFetched}
-					handlePageClick={handlePageClick}
-				/>
-			) : content === "locations" ? (
-				<Locations
-					locations={locationsFetched}
-					handlePageClick={handlePageClick}
-				/>
-			) : (
-				<Infotext />
-			)}
-		</div>
-	)
+      {content === "characters" ? (
+        <Characters
+          characters={charactersFetched}
+          handlePageClick={handlePageClick}
+        />
+      ) : content === "locations" ? (
+        <Locations
+          locations={locationsFetched}
+          handlePageClick={handlePageClick}
+        />
+      ) : (
+        <Infotext />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
